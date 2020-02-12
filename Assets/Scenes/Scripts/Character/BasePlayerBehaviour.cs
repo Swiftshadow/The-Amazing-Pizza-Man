774c08b6720d131c4b6693dc9f623a5ea66b0886
AKrenzel <48369631+AKrenzel@users.noreply.github.com>
2020-02-12 12:34:19 -0600

    Started Adding Folders For Assets and Created some Scripts

---
+++ Assets/Scenes/Scripts/Character/BasePlayerBehaviour.cs
@@ -0,0 +1,27 @@
+﻿/*****************************************************************************
+// File Name :         BasePlayerBehaviour.cs
+// Author :            Andrew Krenzel
+// Creation Date :     #CREATIONDATE#
+//
+// Brief Description : ADD BRIEF DESCRIPTION OF THE FILE HERE
+*****************************************************************************/
+using UnityEngine;
+
+public class BasePlayerBehaviour : MonoBehaviour
+{
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scenes/Scripts/GameControllerBehaviour.cs
@@ -0,0 +1,27 @@
+﻿/*****************************************************************************
+// File Name :         GameControllerBehaviour.cs
+// Author :            Andrew Krenzel
+// Creation Date :     #CREATIONDATE#
+//
+// Brief Description : ADD BRIEF DESCRIPTION OF THE FILE HERE
+*****************************************************************************/
+using UnityEngine;
+
+public class GameControllerBehaviour : MonoBehaviour
+{
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        
+    }
+}
