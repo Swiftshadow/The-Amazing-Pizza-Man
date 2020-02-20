717ff51088187f7d8ab3033afe9c4ed45487922b
Doug Guzman <swiftshadow34@gmail.com>
2020-02-19 18:45:47 -0600

    Adds functional random room generation

--- Assets/Scripts/World/RoomSpawnSettings.cs
+++ Assets/Scripts/World/RoomSpawnSettings.cs
@@ -7,13 +7,22 @@
 *****************************************************************************/
 using UnityEngine;
 
+/// <summary>
+/// Lives on spawnpoint objects childed to the main room
+/// </summary>
 public class RoomSpawnSettings : MonoBehaviour
 {
     
+    [Tooltip("Difficulty of the room to spawn")]
     public EnumList.RoomDifficulty difficulty;
 
+    [Tooltip("The side of the room to spawn that must be open")]
     public EnumList.RoomDoors doorAttachSide;
 
+    /// <summary>
+    /// Adds a door to the list of must open
+    /// </summary>
+    /// <param name="doorToAdd">The side that must be open</param>
     public void AddDoor(EnumList.RoomDoors doorToAdd)
     {
         doorAttachSide = doorAttachSide | doorToAdd;
