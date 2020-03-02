3f79fefbd65813c0791b57991944371da1b218b3
tzarvell9 <tzarvell@mail.bradley.edu>
2020-03-02 12:13:53 -0600

    finalized broccoli following player

--- Assets/Scripts/Enemy/BroccoliMonster.cs
+++ Assets/Scripts/Enemy/BroccoliMonster.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
+/*****************************************************************************
+// File Name :         GameControllerBehaviour.cs
+// Author :            Taylor Zarvell
+// Creation Date :     3/2/20
+//
+// Brief Description : Basic movement of monster 
+*****************************************************************************/
 using UnityEngine;
 
 public class BroccoliMonster : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform player;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+    public float moveSpeed = 3f;
+
+    // get the rigidbody
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    // find position
     void Update()
     {
-        
+        Vector3 direction = player.position - transform.position;
+
+        direction.Normalize();
+        movement = direction; 
+    }
+
+    private void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
     }
 }
