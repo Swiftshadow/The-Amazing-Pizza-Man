8a16da18037235458b492124b2a33937b4dd130f
Doug Guzman <dgguzman@mail.bradley.edu>
2020-03-04 11:40:34 -0600

    Adds 2d extras and better room placeholder prefabs

---
+++ Assets/Scripts/Character/CameraBehaviour.cs
@@ -0,0 +1,40 @@
+/*****************************************************************************
+// File Name :         CameraBehaviour.cs
+// Author :            Doug Guzman
+// Creation Date :     March 04, 2020
+//
+// Brief Description : Controls the camera movement around the scene
+*****************************************************************************/
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+
+    /// <summary>
+    /// Reference to the player
+    /// </summary>
+    private GameObject player;
+
+    [Tooltip("How high above the player the camera should hover")]
+    public float hoverDistance = -10;
+    
+    /// <summary>
+    /// Finds and sets the player reference
+    /// <\summary>
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    /// <summary>
+    /// Moves the camera to be over the player at all times
+    /// <\summary>
+    void Update()
+    {
+        Vector3 targetPos = player.transform.position;
+
+        targetPos.z = hoverDistance;
+
+        transform.position = targetPos;
+    }
+}
