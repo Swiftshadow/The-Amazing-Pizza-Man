36c668dc2dc8e75b7bd2d4ad4178629faf65fb33
tzarvell9 <tzarvell@mail.bradley.edu>
2020-03-30 11:29:11 -0500

    Update ChaseBehaviour.cs

--- Assets/ChaseBehaviour.cs
+++ Assets/ChaseBehaviour.cs
@@ -1,26 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
+/*****************************************************************************
+// File Name :         ChaseBehaviour.cs
+// Author :            Taylor Zarvell 100%
+// Creation Date :     3/29/20
+//
+// Brief Description : Chase state of monster
+*****************************************************************************/
 using UnityEngine;
 
 public class ChaseBehaviour : StateMachineBehaviour
 {
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
 
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    private Transform playerPos;
+    public float speed;
 
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.transform.position = Vector2.MoveTowards(animator.transform.position, playerPos.position, speed *Time.deltaTime);
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
