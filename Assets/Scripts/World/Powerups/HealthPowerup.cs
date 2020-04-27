e3f6d277360608a932a04a6c2a9d72396ea0987c
AKrenzel <48369631+AKrenzel@users.noreply.github.com>
2020-04-27 14:54:07 -0500

    Floaty Power Update

--- Assets/Scripts/World/Powerups/HealthPowerup.cs
+++ Assets/Scripts/World/Powerups/HealthPowerup.cs
@@ -18,4 +18,25 @@ public class HealthPowerup : PowerupBase
             player.health = 100;
         }
     }
+    
+    // AK IR2
+    void Update()
+    {
+        Movement();
+    }
+    
+    // AK IR2
+    public float frequency = 2.5f;
+    public float magnitude = 0.005f;
+    
+    // AK IR2
+    void Movement()
+    {
+        var pos = transform.position;
+
+        Vector3 sinOffset = Vector3.up * Mathf.Sin(Time.time * frequency);
+        sinOffset *= magnitude;
+        
+        transform.position = pos + sinOffset;
+    }
 }
--- Assets/Scripts/World/Powerups/MapPowerup.cs
+++ Assets/Scripts/World/Powerups/MapPowerup.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MapPowerup : PowerupBase
 {
@@ -30,5 +31,26 @@ public class MapPowerup : PowerupBase
 
             //++counter;
         }
+    }
+
+    // AK IR2
+    void Update()
+    {
+        Movement();
+    }
+    
+    // AK IR2
+    public float frequency = 2.5f;
+    public float magnitude = 0.005f;
+    
+    // AK IR2
+    void Movement()
+    {
+        var pos = transform.position;
+
+        Vector3 sinOffset = Vector3.up * Mathf.Sin(Time.time * frequency);
+        sinOffset *= magnitude;
+
+        transform.position = pos + sinOffset;
     }
 }
